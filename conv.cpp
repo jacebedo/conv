4c6e57cf9c4e2e741a6f158cf910e223d944256d
jacebedo <jacebedo@sfu.ca>
2018-01-07 16:58:59 -0800

    Add input structure

--- conv.cpp
+++ conv.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <stdlib.h>
 #include <string>
+#include <iostream>
 #include <sqlite3.h>
 
 
@@ -9,14 +10,16 @@ using namespace std;
 int main(int argc,char *argv[]) {
 
   sqlite3 * db;
-
+  string src = "";
+  string dst = "";
+  float qty = 0.0;
   // sqlite database initialization
   int error_status = sqlite3_open("conversions.db",&db);
   if (error_status) cout << "ERROR: database connection failed\n";
 
 
 
-  if (argc <= 1 || argc > 5) {
+  if (argc <= 1 || argc >= 5) {
     cout << "please enter the correct syntax or use './conv help' for proper use." << "\n";
     return 0;
   }
@@ -27,10 +30,23 @@ int main(int argc,char *argv[]) {
       if (string(argv[i]) == "help") {
         /*print out help documentation */
         cout << "help documentation placeholder \n";
+        return 0;
       }
-
-
     }
+
+    src = argv[1];
+    dst = argv[2];
+    qty = strtof(argv[3],NULL);
+    // To do: Implement test cases, and test program for edge cases.
+
+
+    /* Testing */
+    // cout << "src: " << src << "\n";
+    // cout << "dst: " << dst << "\n";
+    // cout << "qty: " << qty << "\n";
+
+
+
   }
 
 
